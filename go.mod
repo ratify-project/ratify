045bc3c68d6aaaeae633955476971a760a6563d7
Eric Trexel <etrexel1@gmail.com>
2021-11-01 12:48:45 -0700

    Integrate oras-go library

--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/deislabs/hora
 go 1.16
 
 require (
-	github.com/docker/cli v20.10.7+incompatible
 	github.com/docker/distribution v2.7.1+incompatible
 	github.com/google/go-containerregistry v0.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/notaryproject/notation-go-lib v1.0.0-alpha-1
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.1
+	github.com/oras-project/artifacts-spec v1.0.0-draft.1
 	github.com/pkg/errors v0.9.1
 	github.com/sigstore/cosign v1.1.0
 	github.com/sigstore/sigstore v0.0.0-20210729211320-56a91f560f44
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/xlab/treeprint v1.1.0
+	oras.land/oras-go v0.5.1-0.20211103031936-61d91f9abd65
 )
--- pkg/referrerstore/oras/cosign.go
+++ pkg/referrerstore/oras/cosign.go
@@ -3,46 +3,54 @@ package oras
 import (
 	"github.com/deislabs/hora/pkg/common"
 	"github.com/deislabs/hora/pkg/ocispecs"
-	"github.com/deislabs/hora/pkg/referrerstore/oras/registry"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+	"github.com/opencontainers/go-digest"
 	oci "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/sigstore/cosign/pkg/cosign"
+	"net/http"
+	"strings"
 )
 
 const CosignArtifactType = "org.sigstore.cosign.v1"
 
-func getCosignReferences(client *registry.Client, subjectReference common.Reference) (*[]ocispecs.ReferenceDescriptor, error) {
+func getCosignReferences(subjectReference common.Reference) (*[]ocispecs.ReferenceDescriptor, error) {
 	var references []ocispecs.ReferenceDescriptor
+	if strings.Split(subjectReference.Original, "@")[1] == "" {
+		return &references, nil
+	}
 	ref, err := name.ParseReference(subjectReference.Original)
 	if err != nil {
-		return nil, err
+		return &references, err
 	}
 	hash := v1.Hash{
 		Algorithm: subjectReference.Digest.Algorithm().String(),
 		Hex:       subjectReference.Digest.Hex(),
 	}
 	signatureTag := cosign.AttachedImageTag(ref.Context(), hash, cosign.SignatureTagSuffix)
-	tagRef := common.Reference{
-		Path: subjectReference.Path,
-		Tag:  signatureTag.TagStr(),
-	}
-	desc, err := client.GetManifestMetadata(tagRef)
 
-	if err != nil && err != registry.ManifestNotFound {
-		return nil, err
+	desc, err := remote.Get(signatureTag)
+	if terr, ok := err.(*transport.Error); ok && terr.StatusCode == http.StatusNotFound {
+		return &references, nil
+	}
+	if err != nil {
+		return &references, err
+	}
+	descDig, err := digest.Parse(desc.Digest.String())
+	if err != nil {
+		return &references, err
 	}
 
-	if err == nil {
-		references = append(references, ocispecs.ReferenceDescriptor{
-			ArtifactType: CosignArtifactType,
-			Descriptor: oci.Descriptor{
-				MediaType: desc.MediaType,
-				Digest:    desc.Digest,
-				Size:      desc.Size,
-			},
-		})
-	}
+	references = append(references, ocispecs.ReferenceDescriptor{
+		ArtifactType: CosignArtifactType,
+		Descriptor: oci.Descriptor{
+			MediaType: string(desc.MediaType),
+			Digest:    descDig,
+			Size:      desc.Size,
+		},
+	})
 
 	return &references, nil
 }
