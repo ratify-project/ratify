f33ca40b297b90d8931ef449957186490cbabc0d
Susan Shi <huish@microsoft.com>
2022-03-16 10:42:24 -0700

    Support notary verification certification load from directory (#141)
    * adding read from path Util method
    
    * Describe your commit here
    
    * adding error msg
    
    * moving crypto.Cert read into utils method
    
    * renaming file to utils
    
    * renaming to certificateUtil which in future we can add keyvault related logic
    
    * check if crt is nil before appending
    
    Co-authored-by: huish@microsoft.com <azureuser@huishVM.fitk5ac0v4uepfxyp10tia5ysa.xx.internal.cloudapp.net>

---
+++ pkg/utils/certificateUtil_test.go
@@ -0,0 +1,109 @@
+/*
+Copyright The Ratify Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestReadCertificatesFromPath_NestedDirectory(t *testing.T) {
+	// Setup to create nested directory structure
+	testDir := "TestDirectory"
+	nestedDir := ".nestedFolder"
+	testFile1 := testDir + string(os.PathSeparator) + "file1.txt"
+	testFile2 := testDir + string(os.PathSeparator) + nestedDir + string(os.PathSeparator) + ".file2.crt"
+	testFile3 := testDir + string(os.PathSeparator) + "file3.crt"
+	testFile4 := testDir + string(os.PathSeparator) + "file4.crt"
+
+	setupDirectoryForTesting(t, testDir)
+	setupDirectoryForTesting(t, testDir+string(os.PathSeparator)+nestedDir)
+
+	createFile(t, testFile1)
+	createCertFile(t, testFile2)
+	createCertFile(t, testFile3)
+	createCertFile(t, testFile4)
+
+	// Invoke method to test
+	files, err := GetCertificatesFromPath(testDir)
+
+	// Tear down
+	os.RemoveAll(testDir)
+
+	// Validate
+	expectedFileCount := 3
+	if len(files) != expectedFileCount || err != nil {
+		t.Fatalf("response length expected to be %v, actual %v, error %v", expectedFileCount, len(files), err)
+	}
+}
+
+func TestReadFilesFromPath_SingleFile(t *testing.T) {
+	// Setup
+	testDir := "TestDirectory"
+	testFile1 := testDir + string(os.PathSeparator) + "file1.Crt"
+
+	setupDirectoryForTesting(t, testDir)
+	createCertFile(t, testFile1)
+
+	// Invoke method to test
+	files, err := GetCertificatesFromPath(testDir)
+
+	// Teardown
+	os.RemoveAll(testDir)
+
+	// Validate
+	if len(files) != 1 || err != nil {
+		t.Fatalf("response length expected to be 1, actual %v, error %v", len(files), err)
+	}
+
+}
+
+func createFile(t *testing.T, path string) {
+	_, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		t.Fatalf("write file '%s' failed with error '%v'", path, err)
+	}
+}
+
+func createCertFile(t *testing.T, path string) {
+
+	// open cert file
+	content, err := ioutil.ReadFile("testCert1.crt")
+
+	if err != nil {
+		t.Fatalf("open cert file '%s' failed with error '%v'", path, err)
+	}
+
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		t.Fatalf("creating new file '%s' failed with error '%v'", path, err)
+
+	}
+
+	_, err = file.Write(content)
+	if err != nil {
+		t.Fatalf("write file '%s' failed with error '%v'", path, err)
+	}
+
+}
+
+func setupDirectoryForTesting(t *testing.T, path string) {
+	err := os.Mkdir(path, 0755)
+	if err != nil {
+		t.Fatalf("Creating directory '%s' failed with '%v'", path, err)
+	}
+}
