f414cd4c2492d909493e108d3aa2131c0bdedf18
Akash Singhal <akashsinghal@microsoft.com>
2023-03-10 13:08:19 -0800

    test: add more unit tests (#671)

---
+++ pkg/referrerstore/oras/mocks/oras_storage.go
@@ -0,0 +1,50 @@
+/*
+Copyright The Ratify Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mocks
+
+import (
+	"context"
+	"io"
+
+	"github.com/opencontainers/go-digest"
+	oci "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/v2/content"
+	"oras.land/oras-go/v2/errdef"
+)
+
+type TestStorage struct {
+	content.Storage
+	ExistsMap map[digest.Digest]io.Reader
+}
+
+func (s TestStorage) Exists(ctx context.Context, target oci.Descriptor) (bool, error) {
+	if _, ok := s.ExistsMap[target.Digest]; ok {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (s TestStorage) Push(ctx context.Context, expected oci.Descriptor, content io.Reader) error {
+	s.ExistsMap[expected.Digest] = content
+	return nil
+}
+
+func (s TestStorage) Fetch(ctx context.Context, target oci.Descriptor) (io.ReadCloser, error) {
+	if reader, ok := s.ExistsMap[target.Digest]; ok {
+		return io.NopCloser(reader), nil
+	}
+	return nil, errdef.ErrNotFound
+}
