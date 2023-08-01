24d04491ba8c76bb9385e7b02ef2d81786496b5b
huish@microsoft.com <huish@microsoft.com>
2023-08-01 07:31:21 +0000

    fix cleanup script

--- scripts/azure-ci-test-cleanup.sh
+++ scripts/azure-ci-test-cleanup.sh
@@ -17,29 +17,32 @@
 ##--------------------------------------------------------------------
 
 export AZURE_SUBSCRIPTION_ID=${1}
+
 cleanup() {
   az account set -s ${AZURE_SUBSCRIPTION_ID}
   echo "Deleting resource group with ratifye2e tag"
    
   # Get list of resources with specific tag
-  resource_list=`az resource list --tag ratifye2e --subscription ${subscription}`
+  resource_list=`az group list --tag ratifye2e`
 
   # Get number of resources to be deleted
   num_resources=`echo $resource_list | jq length`
   echo 'Found' $num_resources 'resources'
 
   # Delete resources
-  for((i=0; i<$num_resources; i++)); do
+  for((i=0; i<$num_resources; i++)) 
+  do
       # Get $i th resource name and type
-      name=`echo $resource_list | jq .[$i].name | tr -d '"'`
-      type=`echo $resource_list | jq .[$i].type | tr -d '"'`
+     name=`echo $resource_list | jq .[$i].name | tr -d '"'`
+     type=`echo $resource_list | jq .[$i].type | tr -d '"'`
       
       echo 'Deleting following resource ...'
       echo 'resource name': $name
       echo 'resource type': $type
       
       # Delete $i th resource
-      # az resource delete -g ${resource_group} -n ${name} --resource-type ${type}
+      az group delete --name ${name} -y --no-wait
+       
   done
 }
 
