62e920e651e1751418d1182f1dab3494e3bef72d
Binbin Li <libinbin@microsoft.com>
2023-08-30 13:08:01 +0000

    feat: add version and OS/Arch to user-agent header

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,6 +1,17 @@
 package version
 
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+)
+
 var (
+	// Version is the current version of Ratify.
+	Version = ""
+
+	// UserAgent is the user agent for http request header.
+	UserAgent = ""
 
 	//GitTag
 	GitTag = ""
@@ -11,3 +22,23 @@ var (
 	//GitTreeState shows if the tree is unmodified or modified
 	GitTreeState = ""
 )
+
+func init() {
+	UserAgent = generateUserAgent()
+}
+
+func generateUserAgent() string {
+	vcsrevision := "unknown"
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, v := range info.Settings {
+			switch v.Key {
+			case "vcs.revision":
+				vcsrevision = v.Value
+			}
+		}
+	}
+	if Version == "" {
+		Version = vcsrevision
+	}
+	return fmt.Sprintf("%s+%s (%s/%s)", "ratify", Version, runtime.GOOS, runtime.GOARCH)
+}
