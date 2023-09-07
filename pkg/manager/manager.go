3b271b8018904cf424c7f03027829d32b952d907
Susan Shi <huish@microsoft.com>
2023-09-06 20:47:41 -0700

    feat: add health Probe (#1058)
    Signed-off-by: Susan Shi <huish@microsoft.com>

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -142,12 +142,11 @@ func StartServer(httpServerAddress, configFilePath, certDirectory, caCertFile st
 	}
 }
 
-func StartManager(certRotatorReady chan struct{}) {
+func StartManager(certRotatorReady chan struct{}, probeAddr string) {
 	var metricsAddr string
 	var enableLeaderElection bool
-	var probeAddr string
+
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -178,6 +177,8 @@ func StartManager(certRotatorReady chan struct{}) {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
+
+	setupLog.Debugf("setting up probeAddr at %s", probeAddr)
 
 	// Make sure certs are generated and valid if cert rotation is enabled.
 	if featureflag.CertRotation.Enabled {
