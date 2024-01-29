82626ee7b58fbbb79095dde46b46da5cf99247bb
Susan Shi <huish@microsoft.com>
2024-01-29 15:58:06 -0800

    fix: dynamic plugin should support pulling image with digest (#1280)

--- pkg/referrerstore/factory/factory_test.go
+++ pkg/referrerstore/factory/factory_test.go
@@ -95,37 +95,55 @@ func TestCreateStoresFromConfig_PluginStores_ReturnsExpected(t *testing.T) {
 func TestCreateStoresFromConfig_DynamicPluginStores_ReturnsExpected(t *testing.T) {
 	os.Setenv("RATIFY_EXPERIMENTAL_DYNAMIC_PLUGINS", "1")
 	featureflag.InitFeatureFlagsFromEnv()
-	storeConfig := map[string]interface{}{
-		"name": "plugin-store",
-		"source": map[string]interface{}{
-			"artifact": "wabbitnetworks.azurecr.io/test/sample-verifier-plugin:v1",
+	testCases := []struct {
+		name     string
+		artifact string
+	}{
+		{
+			name:     "image specified by tag",
+			artifact: "wabbitnetworks.azurecr.io/test/sample-store-plugin:v1",
+		},
+		{
+			name:     "image specified by digest",
+			artifact: "wabbitnetworks.azurecr.io/test/sample-store-plugin@sha256:96ba9f9636cde32df87d62dcad4e430d055e708b9f173475c5d7468b732d6566",
 		},
 	}
 
-	storesConfig := config.StoresConfig{
-		Stores: []config.StorePluginConfig{storeConfig},
-	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			storeConfig := map[string]interface{}{
+				"name": "plugin-store",
+				"source": map[string]interface{}{
+					"artifact": tc.artifact,
+				},
+			}
 
-	stores, err := CreateStoresFromConfig(storesConfig, "")
+			storesConfig := config.StoresConfig{
+				Stores: []config.StorePluginConfig{storeConfig},
+			}
 
-	if err != nil {
-		t.Fatalf("create stores failed with err %v", err)
-	}
+			stores, err := CreateStoresFromConfig(storesConfig, "")
 
-	if len(stores) != 1 {
-		t.Fatalf("expected to have %d stores, actual count %d", 1, len(stores))
-	}
+			if err != nil {
+				t.Fatalf("create stores failed with err %v", err)
+			}
 
-	if stores[0].Name() != "plugin-store" {
-		t.Fatalf("expected to create plugin store")
-	}
+			if len(stores) != 1 {
+				t.Fatalf("expected to have %d stores, actual count %d", 1, len(stores))
+			}
 
-	if _, ok := stores[0].(*plugin.StorePlugin); !ok {
-		t.Fatalf("type assertion failed expected a plugin store")
-	}
+			if stores[0].Name() != "plugin-store" {
+				t.Fatalf("expected to create plugin store")
+			}
 
-	pluginPath := path.Join(stores[0].GetConfig().PluginBinDirs[0], stores[0].Name())
-	if _, err := os.Stat(pluginPath); errors.Is(err, os.ErrNotExist) {
-		t.Fatalf("downloaded plugin not found in path")
+			if _, ok := stores[0].(*plugin.StorePlugin); !ok {
+				t.Fatalf("type assertion failed expected a plugin store")
+			}
+
+			pluginPath := path.Join(stores[0].GetConfig().PluginBinDirs[0], stores[0].Name())
+			if _, err := os.Stat(pluginPath); errors.Is(err, os.ErrNotExist) {
+				t.Fatalf("downloaded plugin not found in path")
+			}
+		})
 	}
 }
