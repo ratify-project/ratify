c9de31d9efe8485e3c3f37682e1648f99eb53129
Juncheng Zhu <junczhu@microsoft.com>
2024-10-30 23:53:20 +0000

    feat: refactor the interface
    Signed-off-by: Juncheng Zhu <junczhu@microsoft.com>

--- pkg/crlprovider/provider.go
+++ pkg/crlprovider/provider.go
@@ -3,9 +3,7 @@ Copyright The Ratify Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,39 +14,22 @@ limitations under the License.
 package crlprovider
 
 import (
-	"context"
-	"net/http"
-	"sync"
-	"time"
-
 	corecrl "github.com/notaryproject/notation-core-go/revocation/crl"
 )
 
-// static concurrency-safe map to store errors while fetching CRL from CRL management provider.
-// layout:
-//
-// map["<namespace>/<name>"] = error
-var CRLErrMap sync.Map
-
-// GetCRL gets the CRL from the provider
-func GetCRL(ctx context.Context, url string, timeout time.Duration) (*corecrl.Bundle, error) {
-	if err, ok := CRLErrMap.Load(url); ok && err != nil {
-		return nil, err.(error)
-	}
-	newFetcher, err := corecrl.NewHTTPFetcher(&http.Client{Timeout: timeout})
-	if err != nil {
-		return nil, err
-	}
-	bundle, err := newFetcher.Fetch(ctx, url)
-	if err != nil {
-		SetCRLError(url, err)
-		return nil, err
-	}
-	CRLErrMap.Delete(url)
-	return bundle, nil
+type CRLFetcherOptions struct {
+	useCache bool
 }
 
-// SetCRLError sets the error while fetching CRL from CRL management provider.
-func SetCRLError(url string, err error) {
-	CRLErrMap.Store(url, err)
+type CRLCacheOptions struct {
+	cacheRoot string
+}
+
+// CRLProvider defines the interface for managing CRL operations.
+type CRLProvider interface {
+	// NewCRLFetcher creates a new CRLFetcher with the given paramters.
+	NewCRLFetcher(opts CRLFetcherOptions) (corecrl.Fetcher, error)
+
+	// NewCacheCRL caches the CRL using the provided cache provider.
+	NewCRLCache(opts CRLCacheOptions) (corecrl.Cache, error)
 }
