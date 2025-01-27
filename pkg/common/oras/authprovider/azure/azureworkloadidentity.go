632e9cb4ab41a1d9d06e9772cd883abf41b8ba8c
Binbin Li <libinbin050215@gmail.com>
2025-01-27 01:26:10 +0000

    chore: address comments
    Signed-off-by: Binbin Li <libinbin050215@gmail.com>

--- pkg/common/oras/authprovider/azure/azureworkloadidentity.go
+++ pkg/common/oras/authprovider/azure/azureworkloadidentity.go
@@ -18,9 +18,7 @@ package azure
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	azcontainerregistry "github.com/Azure/azure-sdk-for-go/sdk/containers/azcontainerregistry"
@@ -217,57 +215,6 @@ func (d *WIAuthProvider) Provide(ctx context.Context, artifact string) (provider
 	}
 
 	return authConfig, nil
-}
-
-// parseEndpoints checks if the endpoints are valid for auth provider. If no
-// endpoints are provided, it defaults to the default ACR endpoint.
-// A valid endpoint is either a fully qualified domain name or a wildcard domain
-// name folloiwing RFC 1034.
-// Valid examples:
-// - *.example.com
-// - example.com
-//
-// Invalid examples:
-// - *
-// - example.*
-// - *example.com.*
-// - *.
-func parseEndpoints(endpoints []string) ([]string, error) {
-	if len(endpoints) == 0 {
-		return []string{defaultACREndpoint}, nil
-	}
-	for _, endpoint := range endpoints {
-		switch strings.Count(endpoint, "*") {
-		case 0:
-			continue
-		case 1:
-			if !strings.HasPrefix(endpoint, "*.") {
-				return nil, fmt.Errorf("invalid wildcard domain name: %s, it must start with '*.'", endpoint)
-			}
-			if len(endpoint) < 3 {
-				return nil, fmt.Errorf("invalid wildcard domain name: %s, it must have at least one character after '*.'", endpoint)
-			}
-		default:
-			return nil, fmt.Errorf("invalid wildcard domain name: %s, it must have at most one wildcard character", endpoint)
-		}
-	}
-	return endpoints, nil
-}
-
-// validateHost checks if the host is matching endpoints supported by the auth
-// provider.
-func validateHost(host string, endpoints []string) error {
-	for _, endpoint := range endpoints {
-		if endpoint[0] == '*' {
-			if _, zone, ok := strings.Cut(host, "."); ok && zone == endpoint[2:] {
-				return nil
-			}
-		}
-		if host == endpoint {
-			return nil
-		}
-	}
-	return fmt.Errorf("the artifact host %s is not in the scope of the store auth provider", host)
 }
 
 // Compare addExpiry with default ACR refresh token expiry
